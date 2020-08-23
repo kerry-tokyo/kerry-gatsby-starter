15253ce138744746b7691e1cba04831d00c080c5
Kerry <info@svillon.com>
2020-08-23 20:34:35 +0900

    Change: gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `kerry-web`,
+        name: `kerry-gatsby-starter`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
